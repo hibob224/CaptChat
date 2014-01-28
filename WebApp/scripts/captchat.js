a42f73bc62ff324abd6e21bbbc4b4bab12ddc825
Jonathan Miller <jonny224@hotmail.co.uk>
2014-01-28 15:23:52 +0000

    Moar features
    Indentation fixed
    Scrolling fixed
    partnerMessage CSS class added, aligns message to right of screen

--- WebApp/scripts/captchat.js
+++ WebApp/scripts/captchat.js
@@ -1,88 +1,90 @@
 CaptChat = {
-  tCtx: {}, //Canvas context will be assigned here on page load
-  fontSize: "20px",
+	tCtx: {}, //Canvas context will be assigned here on page load
+	fontSize: "20px",
 	runScript: function(e) {
-    if (e.keyCode == 13 && document.getElementById('input').value) {
-      CaptChat.doTheThing();
-      $('#messages').append('<br/>'); //New line between messages
-      $('#messages').animate({ scrollTop: $('#messages').outerHeight() }, 700);
-      document.getElementById('input').value = '';
+ 		if (e.keyCode == 13 && document.getElementById('input').value) {
+			CaptChat.doTheThing();
+			$('#messages').append('<br/>'); //New line between messages
+			$('#messages').animate({ scrollTop: $('#messages').outerHeight() }, 400, 'swing', function() {
+										$('#messages').stop(); //Stop scroll to prevent it affecting user scrolling
+								   });
+			document.getElementById('input').value = '';
 		}
-  },
+	},
 
-  doTheThing: function(input) {
-    input = input || document.getElementById('input').value;
-    input = input.trim().replace(/\s+/g, ' ').split(' ');
+	doTheThing: function(input) {
+		input = input || document.getElementById('input').value;
+		input = input.trim().replace(/\s+/g, ' ').split(' ');
 
-    var message = $('<span/>', { class: 'aMessage' });
+		var message = $('<span/>', { class: 'aMessage' });
 
-    for( var word in input ) {
-      CaptChat.canvas.width(CaptChat.textWidth(input[word])+10);
-      CaptChat.captchaIfy(input[word]);
-      message.append(CaptChat.canvas.toImg());
-      //CaptChat.appendImg(CaptChat.canvas.toImg());
-      CaptChat.canvas.clear();
-    }
-    $('#messages').append(message);
-  },
+		for( var word in input ) {
+			CaptChat.canvas.width(CaptChat.textWidth(input[word])+10);
+			CaptChat.captchaIfy(input[word]);
+			message.append(CaptChat.canvas.toImg());
+			//CaptChat.appendImg(CaptChat.canvas.toImg());
+			CaptChat.canvas.clear();
+		}
+		$('#messages').append(message);
+	},
 
-  captchaIfy: function(input) {
-	  CaptChat.tCtx.font = "normal "+ CaptChat.fontSize +" MomsTypewriter"; //Set text fonts option for  canvas,html5 canvas: Text Option
+	captchaIfy: function(input) {
+		CaptChat.tCtx.font = "normal "+ CaptChat.fontSize +" MomsTypewriter"; //Set text fonts option for  canvas,html5 canvas: Text Option
 		CaptChat.tCtx.strokeStyle = "#000000";				                  //HTML5 canvas: Text Option
 		CaptChat.canvas.clear();                                              //Clear previous Canvas for redraw our captcha
 		CaptChat.tCtx.strokeText(input,0,20,CaptChat.canvas.width());		  //Stroke random string to canvas
 		CaptChat.tCtx.textBaseline = "middle";				                  //HTML5 canvas: Text Option,line in middle of text
 	},
 
-  canvas: {
-    /*****THERE IS BASICALLY NO TYPE CHECKING, ONLY SEND ACTUAL WIDTH/HEIGHT VALUES KTHX*****/
-    //width() returns width, width(width) sets width then returns confirmation
-    width: function(width) {
-      if( arguments.length ) {
-        document.getElementById('textCanvas').width = width;
-      }
-      return document.getElementById('textCanvas').width;
-    },
+	canvas: {
+		/*****THERE IS BASICALLY NO TYPE CHECKING, ONLY SEND ACTUAL WIDTH/HEIGHT VALUES KTHX*****/
+		//width() returns width, width(width) sets width then returns confirmation
+		width: function(width) {
+			if( arguments.length ) {
+				document.getElementById('textCanvas').width = width;
+			}
+			return document.getElementById('textCanvas').width;
+		},
 
-    //height() returns height, height(height) sets height then returns confirmation
-    height: function(height) {
-      if( arguments.length ){
-        document.getElementById('textCanvas').height = height;
-      }
-      return document.getElementById('textCanvas').height;
-    },
+		//height() returns height, height(height) sets height then returns confirmation
+		height: function(height) {
+			if( arguments.length ){
+				document.getElementById('textCanvas').height = height;
+			}
+			return document.getElementById('textCanvas').height;
+		},
 
-    clear: function(tCtx) {
-      tCtx = tCtx || CaptChat.tCtx;
-      tCtx.clearRect(0,0,CaptChat.canvas.width(),CaptChat.canvas.height());
-    },
+		clear: function(tCtx) {
+			tCtx = tCtx || CaptChat.tCtx;
+			tCtx.clearRect(0,0,CaptChat.canvas.width(),CaptChat.canvas.height());
+		},
 
-    toDataURL: function(tCtx) {
-      tCtx = tCtx || CaptChat.tCtx;
-      return tCtx.canvas.toDataURL;
-    },
+		toDataURL: function(tCtx) {
+			tCtx = tCtx || CaptChat.tCtx;
+			return tCtx.canvas.toDataURL;
+		},
 
-    toImg: function(tCtx) {
-      tCtx = tCtx || CaptChat.tCtx;
-      var	imageElem = document.createElement('img');
-      imageElem.src = tCtx.canvas.toDataURL();
+		toImg: function(tCtx) {
+			tCtx = tCtx || CaptChat.tCtx;
+			var	imageElem = document.createElement('img');
+			imageElem.src = tCtx.canvas.toDataURL();
 
-      return imageElem;
-    },
-  },
+			return imageElem;
+		},
+	},
 
-  // Returns actual pixel width of @param:text or everything inside #input if nothing passed
-  textWidth : function(text) {
-    text = text || document.getElementById('input').value;
-    var textSpan = document.getElementById('textSpan');
-    textSpan.style.fontSize = CaptChat.fontSize; //Makes sure we're measuring the right font size
-    textSpan.innerHTML = text;
-    console.log($(textSpan).width())
-    return $(textSpan).width();
-  },
+	// Returns actual pixel width of @param:text or everything inside #input if nothing passed
+	textWidth : function(text) {
+		text = text || document.getElementById('input').value;
+		var textSpan = document.getElementById('textSpan');
+		textSpan.style.fontSize = CaptChat.fontSize; //Makes sure we're measuring the right font size
+		textSpan.innerHTML = text;
+		console.log($(textSpan).width())
+		return $(textSpan).width();
+	},
 
-  appendImg: function(img) {
-	  var messages = document.getElementById('messages');
+	appendImg: function(img) {
+		var messages = document.getElementById('messages');
 		messages.appendChild(img)
-  },
+	},
 };
