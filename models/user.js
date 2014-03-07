991e729ed5bda82f5301c37e4d344a9c284b4353
pl0xy <pl0xinat0r@gmail.com>
2014-03-07 23:28:10 +0000

    Changed Add Contact
    Changed - AddContact now calls a function twice instead of copy/pasted
    code

--- models/user.js
+++ models/user.js
@@ -119,7 +119,7 @@ UserSchema.statics.getAuthenticated = function(username, password, cb) {
 };
 
 UserSchema.statics.addContact = function(user1, user2, cb) { //Add users to each others contacts (and remove from requests if present)
-	this.findOne({ username: user1 }, function (err, user) {
+	function addContact (err, user, user2) {
 		if (err) throw err;
 		if (user.contacts.indexOf(user2) === -1) {
 			if (user.requests.indexOf(user2) >= 0)  //Remove user2 from requests (if there)
@@ -130,19 +130,10 @@ UserSchema.statics.addContact = function(user1, user2, cb) { //Add users to each
 			user.contacts.push(user2);				//Add user2 to contacts
 			user.save();
 		}
-	});
-	this.findOne({ username: user2 }, function (err, user) {
-		if (err) throw err;
-		if (user.contacts.indexOf(user1) === -1) {
-			if (user.requests.indexOf(user1) >= 0)
-				user.requests.splice(user.requests.indexOf(user1), 1);
-			if (user.requesting.indexOf(user1) >= 0)
-				user.requesting.splice(user.requesting.indexOf(user1), 1);
+	}
 
-			user.contacts.push(user1);
-			user.save();
-		}
-	});
+	this.findOne({ username: user1 }, addContact(err, user, user2));
+	this.findOne({ username: user1 }, addContact(err, user, user1));
 };
 
 module.exports = mongoose.model('User', UserSchema);
